2d9e704cfbfe67ba07f9658ce1ce0394f802ab39
brayyyy <brayyyy@msn.com>
2016-06-15 20:28:49 +0200

    je commite encore !

--- construct.js
+++ construct.js
@@ -1,3 +1,20 @@
-/**
- * Created by brayyyy on 15/06/16.
- */
+var mode = 1;
+
+var donMode = new DonnesMode(mode);
+
+var tableau = InitTableau(donMode.nbLin, donMode.nbCol);
+PlacerBombes(tableau, donMode);
+CreerTableGraphique(tableau, donMode);
+
+/*var i= 0, j=0;
+while(i < donMode.nbLin)
+{
+    j=0;
+    while(j < donMode.nbCol)
+    {
+        console.log("Valeur "+ i + " " + j + " : " + tableau[i][j]);
+        j++;
+    }
+    i++;
+}*/
+
--- fonctions.js
+++ fonctions.js
@@ -1,16 +1,103 @@
 
+function DonnesMode(mode)
+{
+    switch(mode)
+    {
+        case 1: this.nbBombes = 10;
+                this.nbCol = 8;
+                this.nbLin = 8;
+            break;
+        case 2: this.nbBombes = 40;
+                this.nbCol = 16;
+                this.nbLin = 16;
+            break;
+        case 3: this.nbBombes = 99;
+                this.nbCol = 30;
+                this.nbLin = 16;
+    }
+}
 
-function PlacerBombes(tableau, nbLin, nbCol)
+function InitTableau(nbLin, nbCol)
 {
     var i, j;
+    var tableau = [];
 
     i=0;
     while(i<nbLin)
     {
-        tableau[i] = 3;
+        tableau[i] = [];
+
+        j=0;
+        while(j<nbCol)
+        {
+            tableau[i][j] = -1;
+            j++;
+        }
 
         i++;
     }
 
     return tableau;
 }
+
+function PlacerBombes(tableau, donMode)
+{
+    var i, j, bombesGen;
+
+    bombesGen = 0;
+    i=0;
+    while(i<donMode.nbLin)
+    {
+        j=0;
+        while(j<donMode.nbCol)
+        {
+            while(bombesGen < donMode.nbBombes)
+            {
+                var col = Math.floor(Math.random() * (donMode.nbCol - 1));
+                var lin = Math.floor(Math.random() * (donMode.nbLin - 1));
+
+                if(tableau[lin][col] === -1)
+                {
+                    tableau[lin][col] = -2;
+                    bombesGen++;
+                }
+            }
+            j++;
+        }
+        i++;
+    }
+}
+
+function CreerTableGraphique(tableau, donMode)
+{
+    var table = document.getElementById("maTable");
+
+    var i, j;
+
+    i=0;
+    while(i<donMode.nbLin)
+    {
+        var tr = document.createElement("tr");
+        j=0;
+        while(j<donMode.nbCol)
+        {
+            var td = document.createElement("td");
+            td.appendChild(document.createTextNode(tableau[i][j]));
+            tr.appendChild(td);
+
+            j++;
+        }
+        table.appendChild(tr);
+        i++;
+    }
+}
+
+
+
+
+
+
+
+
+
+
